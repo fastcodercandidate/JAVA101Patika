31e46e990437f8ef016033a9d374c537fcc92a1b
Gülşah Erkal <112139335+fastcodercandidate@users.noreply.github.com>
2022-10-11 19:14:29 +0300

    Add files via upload

---
+++ Main/src/Main5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args){
+        int km;
+        double perkm= 2.20, total,startPrice=10;
+        Scanner input = new Scanner (System.in);
+        System.out.print("Mesafeyi km cinsinden giriniz:");
+        km=input.nextInt();
+        total=( km*perkm);
+        total=total+startPrice;
+        total=(total<20) ? 20 :total;
+        System.out.print("Toplam tutar:"+total);
+    }
+}
